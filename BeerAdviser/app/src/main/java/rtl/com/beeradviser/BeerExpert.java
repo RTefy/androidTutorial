8747aa5edd2660b3dc50ad0e58181edc19077a54
Tefy <tefy@connectic-mada.com>
2016-07-11 17:39:02 +0300

    first commit

---
+++ BeerAdviser/app/src/main/java/rtl/com/beeradviser/BeerExpert.java
@@ -0,0 +1,21 @@
+package rtl.com.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nellistudio on 11/07/16.
+ */
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<String>();
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
